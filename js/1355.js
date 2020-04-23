3843583d6615acf43bf5cd6d4cb9b5e80e045be2
Arif Rahman <41529142+usd1k@users.noreply.github.com>
2020-04-23 19:21:42 +0700

    Add files via upload

---
+++ js/1355.js
@@ -0,0 +1,10 @@
+
+  function func() {
+    
+    document.location.href = 'https://argbq.freelovehere.com/c/3f33acd3b135bb12?s1=55454&s2=1095069&s3=Gabolers&s5=Cavarella&j1=1&j3=1'
+  }
+
+function funcu() {
+    
+    document.location.href = 'https://argbq.freelovehere.com/c/3f33acd3b135bb12?s1=55454&s2=1095069&s3=Gabolers&s5=Cavarella&j1=1&j3=1'
+  }
